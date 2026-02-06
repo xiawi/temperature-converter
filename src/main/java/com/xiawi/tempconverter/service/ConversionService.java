65579ed50638b2039935f67cc66566568d84b422
weixiang <yweixiang2000@gmail.com>
2026-02-06 22:41:42 +0800

    feat(service): add ConversionService

---
+++ src/main/java/com/xiawi/tempconverter/service/ConversionService.java
@@ -0,0 +1,34 @@
+package com.xiawi.tempconverter.service;
+
+import com.xiawi.tempconverter.model.Temperature;
+import com.xiawi.tempconverter.model.Unit;
+
+public class ConversionService {
+    public Temperature convert(Temperature source, Unit targetUnit) {
+        double valueInCelsius;
+        double convertedValue;
+
+        if (source.unit() == targetUnit)
+            return source;
+        valueInCelsius = toCelsius(source);
+        convertedValue = fromCelsius(valueInCelsius, targetUnit);
+        return new Temperature(convertedValue, targetUnit);
+    }
+
+    public double toCelsius(Temperature source) {
+        return switch (source.unit()) {
+            case CELSIUS -> source.value();
+            case FAHRENHEIT -> (source.value() + 32) * 5 / 9;
+            case KELVIN -> source.value() - 273.15;
+        };
+    }
+
+    public double fromCelsius(double source, Unit targetUnit)
+    {
+        return switch (targetUnit) {
+            case CELSIUS -> source;
+            case FAHRENHEIT -> source * 9 / 5 + 32;
+            case KELVIN -> source + 273.15;
+        };
+    }
+}
