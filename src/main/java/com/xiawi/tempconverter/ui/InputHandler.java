4834d44680e499e95d3d1f4bc0f5c49c27b3726d
weixiang <yweixiang2000@gmail.com>
2026-02-07 18:46:12 +0800

    refactor(ui): refactor InputHandler

--- src/main/java/com/xiawi/tempconverter/ui/InputHandler.java
+++ src/main/java/com/xiawi/tempconverter/ui/InputHandler.java
@@ -3,22 +3,72 @@ package com.xiawi.tempconverter.ui;
 import com.xiawi.tempconverter.model.Temperature;
 import com.xiawi.tempconverter.model.Unit;
 
+import java.util.Scanner;
+
 public class InputHandler {
-    public Temperature parseTemperature(String valueString, String unitString) {
+    private Scanner scanner;
+
+    public InputHandler() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int readMenuChoice() {
+        String input;
+
+        while (true) {
+            System.out.println("Choose an Option:");
+            input = scanner.nextLine();
+            try {
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+            }
+        }
+    }
+
+    public Temperature readTemperature() {
         double value;
         Unit unit;
 
-        value = Double.parseDouble(valueString);
-        unit = parseUnit(unitString);
-        return new  Temperature(value, unit);
+        value = readDouble("Enter temperature value:");
+        unit = readUnit("Enter unit (C/F/K):");
+        return new Temperature(value, unit);
     }
 
-    public Unit parseUnit(String unitString) {
-        return switch(unitString) {
-            case "C" -> Unit.CELSIUS;
-            case "K" -> Unit.KELVIN;
-            case "F" -> Unit.FAHRENHEIT;
-            default -> throw new IllegalArgumentException("Invalid unit: " + unitString);
-        };
+    public Unit readTargetUnit() {
+        return readUnit("Convert to unit (C/F/K):");
+    }
+
+    public double readDouble(String prompt){
+        String input;
+
+        while (true) {
+            System.out.println(prompt);
+            input = scanner.nextLine();
+            try {
+                return Double.parseDouble(prompt);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number. Try again.");
+            }
+        }
+    }
+
+    public Unit readUnit(String prompt) {
+        String input;
+
+        while (true) {
+            System.out.println(prompt);
+            input = scanner.nextLine();
+            try {
+                return switch (input) {
+                    case "C" -> Unit.CELSIUS;
+                    case "F" -> Unit.FAHRENHEIT;
+                    case "K" -> Unit.KELVIN;
+                    default -> throw new IllegalArgumentException();
+                };
+            } catch (Exception e) {
+                System.out.println("Invalid Unit. Use C, F, or K.");
+            }
+        }
     }
 }
